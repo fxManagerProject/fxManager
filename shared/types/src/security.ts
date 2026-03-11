1448c974a79a66f260500022d0cb2a3fa88b5f75
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-11 17:15:48 +0100

    feat(shared/types): user permission bit field enum

--- shared/types/src/security.ts
+++ shared/types/src/security.ts
@@ -1,3 +1,22 @@
+export enum UserPermissions {
+  NONE = 0,
+  KICK = 1 << 0, // 1
+  BAN = 1 << 1, // 2
+  WARN = 1 << 2, // 4
+  REVOKE_KICK = 1 << 3, // 8
+  REVOKE_BAN = 1 << 4, // 16
+  REVOKE_WARN = 1 << 5, // 32
+
+  WHITELIST = 1 << 6, // 64
+  REVOKE_WHITELIST = 1 << 7, // 128
+
+  VIEW_REPORT = 1 << 8, // 256
+  SEND_REPORT = 1 << 9, // 512
+  CLOSE_REPORT = 1 << 10, // 1024
+
+  MASTER = 1 << 30, // (1,073,741,824)
+}
+
 export type AuditAction =
   | 'server.start'
   | 'server.stop'
