58eda64ad7c50e932ac5df8826736c1ab84f2d41
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-13 23:54:45 +0100

    chore(resource): clear greetings - bye bye

--- packages/resource/src/common/index.ts
+++ packages/resource/src/common/index.ts
@@ -1,9 +1,1 @@
-import Locale from 'locale';
-import { ResourceContext } from './resource';
 
-export function Greetings() {
-  const greetings = Locale('hello');
-
-  console.log(`started dist/${ResourceContext}.js`);
-  console.log(greetings);
-}
