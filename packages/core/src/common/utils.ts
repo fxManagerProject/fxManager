0e5a9d1f389684dd9704b45209582f939b48c38d
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-14 23:37:57 +0100

    fix(core/common): skip version check in dev mode

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -1,15 +1,10 @@
 import type { ServerConfig } from '@fxmanager/types';
-import { join } from 'node:path';
 
 export const isDev = process.env.NODE_ENV === 'development';
 
-export async function getCoreVersion(): Promise<ServerConfig['version']> {
+export async function getVersion(): Promise<ServerConfig['version']> {
   if (isDev) {
-    const path = join(import.meta.dirname, '..', '..', '..', '..', 'package.json');
-    const file = Bun.file(path);
-
-    const pkg = await file.json();
-    return pkg.version;
+    return 'dev-build';
   } else {
     return (process.env.VERSION as ServerConfig['version']) ?? '0.0.0';
   }
