624b0a22604deb4d1b86f9d74e7a9313537d5fc8
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-30 23:26:43 +0200

    feat(shared): permission manager & export of utils group

---
+++ packages/shared/src/utils/permissions.ts
@@ -0,0 +1,28 @@
+import type { UserPermissionsType } from '@fxmanager/shared/types';
+import { UserPermissions } from '@fxmanager/shared/constants';
+
+export class PermissionManager {
+  constructor() {}
+
+  static has(userBitfield: number, required: UserPermissionsType): boolean {
+    if (userBitfield & UserPermissions.MASTER) return true;
+
+    return (userBitfield & required) === required;
+  }
+
+  static hasAll(userBitfield: number, required: UserPermissionsType[]): boolean {
+    if (userBitfield & UserPermissions.MASTER) return true;
+
+    const combined = required.reduce((acc, p) => acc | p, 0);
+
+    return (userBitfield & combined) === combined;
+  }
+
+  static grant(userBitfield: number, toAdd: UserPermissionsType): number {
+    return userBitfield | toAdd;
+  }
+
+  static revoke(userBitfield: number, toRemove: UserPermissionsType): number {
+    return userBitfield & ~toRemove;
+  }
+}
