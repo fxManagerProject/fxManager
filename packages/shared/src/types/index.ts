38001e7f806ce1e85121cbaf41ed77301b334f8f
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-30 16:23:32 +0200

    fix(packages/types): missing moderation types inferred from db schema

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './api';
 export * from './core';
 export * from './game';
+export * from './moderation';
 export * from './process-manager';
 export * from './players';
 export * from './security';
