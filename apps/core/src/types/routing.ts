ec3590f53d6e2218469e4e05e487d643cb150b7c
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-31 16:46:10 +0200

    chore(core/types): extend FastifyRequest to include admin data

--- apps/core/src/types/routing.ts
+++ apps/core/src/types/routing.ts
@@ -1,6 +1,17 @@
 import type { FastifyPluginAsync } from "fastify";
+import 'fastify';
 
 export interface RouteModule {
 	prefix: string;
 	handler: FastifyPluginAsync;
+}
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    admin?: {
+      id: number;
+      username: string;
+      permissions: number;
+    };
+  }
 }
