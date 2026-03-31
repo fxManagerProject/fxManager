9098e00efc910802719bd204bce71078a6a53172
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-31 15:54:33 +0200

    chore(shared): biome cleanup

--- packages/shared/src/utils/permissions.ts
+++ packages/shared/src/utils/permissions.ts
@@ -2,27 +2,30 @@ import type { UserPermissionsType } from '@fxmanager/shared/types';
 import { UserPermissions } from '@fxmanager/shared/constants';
 
 export class PermissionManager {
-  constructor() {}
+	constructor() {}
 
-  static has(userBitfield: number, required: UserPermissionsType): boolean {
-    if (userBitfield & UserPermissions.MASTER) return true;
+	static has(userBitfield: number, required: UserPermissionsType): boolean {
+		if (userBitfield & UserPermissions.MASTER) return true;
 
-    return (userBitfield & required) === required;
-  }
+		return (userBitfield & required) === required;
+	}
 
-  static hasAll(userBitfield: number, required: UserPermissionsType[]): boolean {
-    if (userBitfield & UserPermissions.MASTER) return true;
+	static hasAll(
+		userBitfield: number,
+		required: UserPermissionsType[],
+	): boolean {
+		if (userBitfield & UserPermissions.MASTER) return true;
 
-    const combined = required.reduce((acc, p) => acc | p, 0);
+		const combined = required.reduce((acc, p) => acc | p, 0);
 
-    return (userBitfield & combined) === combined;
-  }
+		return (userBitfield & combined) === combined;
+	}
 
-  static grant(userBitfield: number, toAdd: UserPermissionsType): number {
-    return userBitfield | toAdd;
-  }
+	static grant(userBitfield: number, toAdd: UserPermissionsType): number {
+		return userBitfield | toAdd;
+	}
 
-  static revoke(userBitfield: number, toRemove: UserPermissionsType): number {
-    return userBitfield & ~toRemove;
-  }
+	static revoke(userBitfield: number, toRemove: UserPermissionsType): number {
+		return userBitfield & ~toRemove;
+	}
 }
