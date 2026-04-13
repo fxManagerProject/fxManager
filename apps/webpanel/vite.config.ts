8b2071b504860df221dd1f1145f4d244fa094377
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-14 00:14:29 +0200

    fix(webpanel/vite.config): proxy settings for ws routes

--- apps/webpanel/vite.config.ts
+++ apps/webpanel/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 				target: 'http://localhost:3000',
 				changeOrigin: true,
 			},
-			'/ws': {
+			'/api/ws': {
 				target: 'ws://localhost:3000',
 				ws: true,
 			},
