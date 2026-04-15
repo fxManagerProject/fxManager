db18fbf6263e7eb80e8682e17699279c15139a1e
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-15 19:38:51 +0200

    feat(core/routes): added ws listener for console commands

--- apps/core/src/routes/api/sockets.ts
+++ apps/core/src/routes/api/sockets.ts
@@ -1,10 +1,11 @@
 import { randomUUID } from 'crypto';
 import { wsManager } from '../../modules/ws.manager';
 import type { RouteModule } from '../../types';
+import type { ProcessOutputLine } from '@fxmanager/shared/types';
 
 // ToDo: add authentication checks
 
-const wsEndpoints: RouteModule['handler'] = async (fastify) => {
+const wsEndpoints: RouteModule['handler'] = async (fastify, { pm }) => {
   fastify.get('', { websocket: true }, (socket, request) => {
     const clientId = randomUUID();
     wsManager.add(clientId, socket);
@@ -12,6 +13,24 @@ const wsEndpoints: RouteModule['handler'] = async (fastify) => {
     // Send client its assigned id so it can reference itself
     socket.send(JSON.stringify({ type: 'connected', clientId }));
   });
+
+	wsManager.on<{ command: string }>('console', 'command', (clientId, event, { command }) => {
+		if (command.includes("resource-api-token") || command.includes("api-port")) {
+			wsManager.send<ProcessOutputLine>(clientId, {
+				channel: 'console',
+				event: 'line',
+				data: {
+          line: '\x1b[31m[           fxManager] protected convar - \x1b[1maction denied\x1b[0m',
+          source: 'stderr',
+          ts: Date.now(),
+				},
+			});
+
+			return;
+		}
+
+		pm.sendCommand(command);
+	});
 };
 
 export default {
