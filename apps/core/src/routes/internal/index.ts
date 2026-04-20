4704ca21bcad491ecd3c07c694b40d2e5f021b8e
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-20 15:56:37 +0200

    feat(core/routes): player endpoints for internal use

--- apps/core/src/routes/internal/index.ts
+++ apps/core/src/routes/internal/index.ts
@@ -1,10 +1,11 @@
+import PlayerModule from "./players";
 import type { RouteModule } from "../../types";
 
 const internalRoutes: RouteModule['handler'] = async (fastify, options) => {
-	// fastify.register(AuthModule.handler, { 
-	// 	...options, 
-	// 	prefix: AuthModule.prefix 
-	// });
+	fastify.register(PlayerModule.handler, { 
+		...options, 
+		prefix: PlayerModule.prefix 
+	});
 };
 
 export default internalRoutes;
---
+++ apps/core/src/routes/internal/players.ts
@@ -0,0 +1,41 @@
+import type { PlayerIdentifiers, PlayerUpdatePackage } from "@fxmanager/shared/types";
+import { resourceAuth } from "../../middleware/resource";
+import type { RouteModule } from "../../types";
+
+const PlayerEndpoints: RouteModule['handler'] = async (fastify, options) => {
+  const { gm } = options;
+
+  fastify.addHook('preHandler', resourceAuth);
+
+  fastify.post('/deferrals', (request) => {
+    const { body } = request;
+
+    return gm.playerDeferralChecks((body as { identifiers: PlayerIdentifiers }).identifiers);
+  });
+
+  fastify.post('/join', (request) => {
+    const { body } = request;
+
+    gm.playerJoin(body as { identifiers: PlayerIdentifiers, serverId: number, name: string });
+    return { ack: true };
+  });
+
+  fastify.post('/drop', (request) => {
+    const { body } = request;
+
+    gm.playerDrop((body as { serverId: number }).serverId);
+    return { ack: true };
+  });
+
+  fastify.post('/update', (request) => {
+    const { body } = request;
+
+    gm.playerUpdates(body as PlayerUpdatePackage);
+    return { ack: true };
+  });
+}
+
+export default {
+  prefix: '/players',
+  handler: PlayerEndpoints,
+} satisfies RouteModule;
