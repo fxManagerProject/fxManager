6c93953efcac5d9d12ed31710b313827a9272d4f
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-23 23:31:42 +0200

    feat(shared/types): create admin user interface

--- packages/shared/src/types/settings.ts
+++ packages/shared/src/types/settings.ts
@@ -8,3 +8,9 @@ export interface BaseAdminUser {
 	createdAt: Date;
 	lastLoginAt: Date | null;
 }
+
+export interface CreateAdminForm {
+	username: string;
+	permissions: UserPermissionsType;
+	playerId: number | null;
+}
