a91c9cd0bba448afd7516817cc55bf6efe8e7a28
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-24 19:09:41 +0200

    chore(packages/shared): update CoreConfig interface

--- packages/shared/src/types/core.ts
+++ packages/shared/src/types/core.ts
@@ -2,12 +2,6 @@ export type PlatformOS = 'windows' | 'linux' | 'unknown';
 
 export interface CoreConfig {
 	platform: PlatformOS;
-	executable: string;
-	serverDataPath: string;
-	configFile: string;
-	autoRestart: boolean;
-	maxRestarts: number;
-	restartDelayMs: number;
 	webServerPort: number;
 	resourceApiToken: string;
 	cookieSecret: string;
