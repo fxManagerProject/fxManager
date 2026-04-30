a9eabe7c98320a29c97b4d0a301a9fcc1105bb18
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-30 22:37:00 +0200

    fix(webpanel/components): header mobile open sidebar button being a div not a button

--- apps/webpanel/src/components/page-header.tsx
+++ apps/webpanel/src/components/page-header.tsx
@@ -15,16 +15,17 @@ export function PageHeader({
 	return (
 		<header className="space-y-1">
 			<div className="flex items-center gap-3">
-				<div
-					className="group/logo md:hidden flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground 
-                     transition-all duration-300 cursor-pointer hover:scale-105 hover:shadow-lg hover:shadow-primary/40"
-					onClick={toggleSidebar}
-				>
+				<button
+          type="button"
+          className="group/logo md:hidden flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground 
+                    transition-all duration-300 cursor-pointer hover:scale-105 hover:shadow-lg hover:shadow-primary/40"
+          onClick={toggleSidebar}
+        >
 					<div className="relative flex items-center justify-center">
 						<PanelLeftIcon className="size-5 transition-all duration-300 group-hover/logo:opacity-0 group-hover/logo:scale-50" />
 						<PanelLeftOpen className="size-5 absolute opacity-0 scale-50 transition-all duration-300 group-hover/logo:opacity-100 group-hover/logo:scale-100" />
 					</div>
-				</div>
+				</button>
 
 				<div className="flex items-center gap-2">
 					<Icon className="text-primary h-6 w-6" />
