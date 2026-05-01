8e0837e8139396e0cfb5c853067cca99f172b5e9
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-01 17:51:21 +0200

    feat(webpanel/hooks): use resourcelist hook

---
+++ apps/webpanel/src/hooks/ws-channels/use-resourcelist.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { useWSBase } from './use-ws-core';
+import type {
+  ResourceData,
+  ResourceInitialData,
+} from '@fxmanager/shared/types';
+
+interface UseResourcelistReturn {
+	resources: ResourceData[];
+	loading: boolean;
+	status: boolean | "errored";
+}
+
+export function useResourcelistSocket(): UseResourcelistReturn {
+	const { subscribe, unsubscribe, on } = useWSBase();
+	const [loading, setLoading] = useState<boolean>(true);
+	const [status, setStatus] = useState<UseResourcelistReturn['status']>(false);
+	const [resources, setResources] = useState<ResourceData[]>([]);
+
+	useEffect(() => {
+		subscribe('playerlist');
+
+		const offSync = on<ResourceInitialData>('resourcelist', 'initial', ({ data }) => {
+			setResources(data.resourcelist);
+      setStatus(data.status);
+			setLoading(false);
+		});
+
+		const offRefresh = on<ResourceInitialData>('resourcelist', 'refresh', ({ data }) => {
+			setResources(data.resourcelist);
+      setStatus(data.status);
+			setLoading(false);
+		});
+
+		const offUpdate = on<ResourceData>(
+			'resourcelist',
+			'update',
+			({ data }) => {
+				setResources((prev) => {
+          const idx = prev.findIndex(res => res.name === data.name);
+
+          if (idx < 0) {
+            return [...prev, data];
+          } else {
+            prev[idx] = data;
+            return prev;
+          }
+        });
+			},
+		);
+
+		return () => {
+			offSync();
+			offRefresh();
+			offUpdate();
+			unsubscribe('resourcelist');
+		};
+	}, [subscribe, unsubscribe, on]);
+
+	return {
+		resources,
+		loading,
+    status,
+	};
+}
