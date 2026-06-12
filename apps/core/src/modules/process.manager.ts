6524a0e32b6c8166588a1dc5a9e65eedb4c271b8
Maximus7474 <the.maximus.7474@gmail.com>
2026-06-12 09:43:17 +0200

    chore(core/modules): move buffer manager to buffer sub directory

--- apps/core/src/modules/process.manager.ts
+++ apps/core/src/modules/process.manager.ts
@@ -3,7 +3,7 @@ import type {
 	ProcessState,
 	ServerState,
 } from '@fxmanager/shared/types';
-import { LogBuffer } from './buffer.manager';
+import { LogBuffer } from './buffer/manager';
 import { ConfigManager } from './config/manager';
 import { wsManager } from './ws.manager';
 import { resourceManager } from './resource.manager';
