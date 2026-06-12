0072e9ebaa5a3457144740365fbf98c3cc8450bf
Maximus7474 <the.maximus.7474@gmail.com>
2026-06-12 10:09:56 +0200

    feat(core/modules): discord manager tests

---
+++ apps/core/src/modules/discord/test.setup.ts
@@ -0,0 +1,32 @@
+import { mock } from 'bun:test';
+
+const mockLogin = mock(async () => 'mock-token');
+const mockDestroy = mock(async () => {});
+const mockGuildsFetch = mock(async () => ({}));
+
+mock.module('discord.js', () => {
+  return {
+    GatewayIntentBits: { Guilds: 1, GuildMembers: 2 },
+    Events: { ClientReady: 'ready' },
+    Client: class MockClient {
+      login = mockLogin;
+      destroy = mockDestroy;
+      guilds = { fetch: mockGuildsFetch };
+      
+      private listeners: Record<string, Function[]> = {};
+      once(event: string, callback: Function) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(callback);
+      }
+      emit(event: string, ...args: any[]) {
+        if (this.listeners[event]) {
+          this.listeners[event].forEach((cb) => {
+						cb(...args)
+					});
+        }
+      }
+    },
+  };
+});
+
+export { mockLogin, mockDestroy, mockGuildsFetch };
