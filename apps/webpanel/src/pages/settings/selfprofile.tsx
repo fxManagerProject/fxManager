4821dbdbad41889346f9afbdf300d4de89274654
Maximus7474 <the.maximus.7474@gmail.com>
2026-07-23 10:59:57 +0200

    fix(panel/settings): profile page not allowing perm editor to overflow

--- apps/webpanel/src/pages/settings/selfprofile.tsx
+++ apps/webpanel/src/pages/settings/selfprofile.tsx
@@ -571,7 +571,7 @@ export default function ProfilePage() {
 								</CardTitle>
 							</CardHeader>
 
-							<CardContent className="flex-1 overflow-hidden">
+							<CardContent className="flex-1 flex flex-col min-h-0 overflow-hidden">
 								<PermissionEditor
 									editable={false}
 									adminId={String(profileData.id)}
